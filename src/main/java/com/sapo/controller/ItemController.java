450ad0620e76828990a64e041cca6de274256499
phunghaduong99 <33689476+phunghaduong99@users.noreply.github.com>
2019-08-17 12:21:38 +0700

    081719

--- src/main/java/com/sapo/controller/ItemController.java
+++ src/main/java/com/sapo/controller/ItemController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import com.sapo.model.Item;
 import com.sapo.repository.CategoryRepository;
 import com.sapo.repository.ItemRepository;
 import com.sapo.shopee.ShopeeApi;
+@CrossOrigin()
 
 @RestController
 public class ItemController {
--- src/main/java/com/sapo/util/Util.java
+++ src/main/java/com/sapo/util/Util.java
@@ -4,7 +4,7 @@ public class Util {
 	public static final String USER_AGENT = "Mozilla/5.0";
 	public static final int PARTNER_ID = 842939;
 	public static final String KEY = "4206e30ba3bdcb3164fe8ade8d4b1f2ed57140a480d32b9bed8af202f65a5e80";
-	public static final String redirectURL="http://192.168.36.28:8081";
+	public static final String redirectURL="192.168.0.103:8081";
 	public static final String secret="spateam1";
 	public static final String GetItemList_URLV1 = "https://partner.shopeemobile.com/api/v1/items/get";
 	public static final String GetItemDetail_URLV1 = "https://partner.shopeemobile.com/api/v1/item/get";
